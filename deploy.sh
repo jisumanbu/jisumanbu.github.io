823165f8a404c9751f3f8e83c6aefb71100c0925
极速漫步 <jisumanbu@163.com>
2018-04-03 15:07:21 +0800

    rename markdown files as english name

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+hexo clean
 hexo g
+hexo d
 
 git add .
 #DATE=`date +%Y-%m-%d.%T`
 git commit -m "rename markdown files as english name"
 git push luodifz@www.bbetterman.com:/data/gitLibrary/jisumanbu.git hexo
 
-hexo d
 
