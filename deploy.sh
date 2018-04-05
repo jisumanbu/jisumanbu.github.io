8f91240446fb888556371369f5309fd669744a03
极速漫步 <jisumanbu@163.com>
2018-04-05 22:49:11 +0800

    怎样才能生活得更好-奇特的一生读书笔记

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ hexo d
 
 git add .
 #DATE=`date +%Y-%m-%d.%T`
-git commit -m "refactor 'RocketMQ二安装与简单实用'"
+git commit -m "怎样才能生活得更好-奇特的一生读书笔记"
 git push luodifz@www.bbetterman.com:/data/gitLibrary/jisumanbu.git hexo
 
 
