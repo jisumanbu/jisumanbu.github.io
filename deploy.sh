09323e2e741cab93e834547bbc82df2b9bca2758
极速漫步 <jisumanbu@163.com>
2018-04-07 14:56:24 +0800

    怎样才能生活得更好-读奇特的一生

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ hexo d
 
 git add .
 #DATE=`date +%Y-%m-%d.%T`
-git commit -m "rebuild after remove _config.yml from repository"
+git commit -m "怎样才能生活得更好-读奇特的一生"
 git push https://github.com/jisumanbu/jisumanbu.github.io.git hexo
 
 
