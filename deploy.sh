7b6dfc3428dabb3026a878340b56081537d065b1
极速漫步 <jisumanbu@163.com>
2018-04-17 10:38:23 +0800

    add Microservices_Designing_Deploying.pdf for download

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ hexo d
 
 git add .
 #DATE=`date +%Y-%m-%d.%T`
-git commit -m "MongoDB从选型到落地到性能调优"
+git commit -m "add Microservices_Designing_Deploying.pdf for download"
 git push https://github.com/jisumanbu/jisumanbu.github.io.git hexo
 
 
