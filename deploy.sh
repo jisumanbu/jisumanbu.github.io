b36580a1c5e39cb7345aea37b00e85dc73ef3e06
jisumanbu <jisumanbu@163.com>
2019-02-13 14:41:15 +0800

    年终总结之2018 - 目标驱动元年

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ hexo clean
 hexo g
 hexo d
 
-git add .
+git add -A .
 #DATE=`date +%Y-%m-%d.%T`
 git commit -m "年终总结之2018 - 目标驱动元年"
 git push https://github.com/jisumanbu/jisumanbu.github.io.git hexo
